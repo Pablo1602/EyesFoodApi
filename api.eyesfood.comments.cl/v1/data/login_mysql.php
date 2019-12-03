e4935af8e7d63d0ab09883e7d4945f8522f1684a
Unknown <pablo.cancino@usach.cl>
2019-12-03 01:18:36 -0300

    fix hostname database

--- api.eyesfood.comments.cl/v1/data/login_mysql.php
+++ api.eyesfood.comments.cl/v1/data/login_mysql.php
@@ -4,7 +4,7 @@
  * Mysql.
  */
 // TODO: cambiar por dominio o IP en producción
-define("MYSQL_HOST", "s-cdbr-iron-east-05.cleardb.net");
+define("MYSQL_HOST", "us-cdbr-iron-east-05.cleardb.net");
 define("MYSQL_DATABASE_NAME", "heroku_381c12ffe1e1d28");
 // TODO: cambiar en producción
 define("MYSQL_USERNAME", "bf2ad6a2d8956c");
