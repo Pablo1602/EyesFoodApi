7d4771dea0df679e0a017dd5dc1a5c3327c037c3
Pablo <pablo.cancino@usach.cl>
2020-08-18 09:33:08 -0400

    test2

--- api.eyesfood.cl/v1/controllers/search.php
+++ api.eyesfood.cl/v1/controllers/search.php
@@ -10,7 +10,7 @@ class search
 
         // TODO: 2. Verificaciones, restricciones, defensas
         //?????????????????????????????????????????????????
-        if (isset($urlSegments[5])) {
+        if (isset($urlSegments[3])) {
             throw new ApiException(
                 400,
                 0,
@@ -22,13 +22,13 @@ class search
         //Hacer switch case para encontrar la URL tipo foods/codigodeBarra/aditivos
         //barcode=urlSegments[0], aditivos e ingredientes=urlSegments[1]
         
-        if (isset($urlSegments[4])) {
+        /*if (isset($urlSegments[4])) {
             switch ($urlSegments[0]){
                 case "noallergy":
                     return self::retrieveSearchAllergy($urlSegments[1],$urlSegments[2],$urlSegments[3]);
                     break;
             }         
-        }
+        }*/
         if (isset($urlSegments[2])) {
             switch ($urlSegments[0]){
                 case "foods":
@@ -147,13 +147,13 @@ private static function retrieveSearchAllergy($leche,$gluten,$query)
         try {
             $pdo = MysqlManager::get()->getDb();
             
-            /*if($leche == 1 and $gluten == 1){
+            if($leche == 1 and $gluten == 1){
                 $comando = "SELECT * "
                         . "FROM  alimentos"
                         . " WHERE nombreAlimento LIKE ? AND alergenos NOT LIKE '%gluten%' AND trazas NOT LIKE '%gluten% AND alergenos NOT LIKE '%leche%' AND trazas NOT LIKE '%leche%' LIMIT 50";
                 // Preparar sentencia
                 $sentencia = $pdo->prepare($comando);
-            }*/
+            }
             else if($leche == 1){
                 $comando = "SELECT * "
                         . "FROM  alimentos"
