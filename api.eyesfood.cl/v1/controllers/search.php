a27a4b5bd224648ef8740b2e0567af72e2f629cf
Pablo <pablo.cancino@usach.cl>
2020-08-18 00:57:50 -0400

    Hotfix 3

--- api.eyesfood.cl/v1/controllers/search.php
+++ api.eyesfood.cl/v1/controllers/search.php
@@ -99,24 +99,22 @@ class search
         }
     }
     
-    private static function retrieveSearchFoods($query)
+ private static function retrieveSearchAdditives($query)
     {
         try {
             $pdo = MysqlManager::get()->getDb();
 
-            /*$comando = "SELECT codigoBarras AS codigo, nombre FROM alimentos"
-                    . " WHERE nombre LIKE ? LIMIT 50";*/
+            $comando = "SELECT codigoE AS codigo, aditivo AS nombre FROM aditivos"
+                    . " WHERE codigoE LIKE ? OR aditivo LIKE ? OR codigoEBuscador LIKE ? LIMIT 50";
 
-            $comando = "SELECT codigoBarras, nombre, marcas.nombreMarca, idUsuario, "
-                        . "idPeligroAlimento, peligroAlimento, productos.producto, "
-                        . "unidades_medida.unidadMedida, contenidoNeto, energia, proteinas, "
-                        . "grasaTotal, grasaSaturada, grasaTrans, colesterol, grasaMono, grasaPoli, "
-                        . "hidratosCarbono, azucaresTotales, fibra, sodio, porcion, porcionGramos, "
-                        . "fechaSubida, indiceGlicemico, fotoOficial FROM alimentos "
-                        . "LEFT JOIN marcas ON alimentos.codigoMarca = marcas.codigoMarca "
-                        . "LEFT JOIN productos ON alimentos.idProducto = productos.idProducto "
-                        . "LEFT JOIN unidades_medida ON alimentos.idUnidadMedida = unidades_medida.idUnidadMedida "
-                        . "WHERE nombre LIKE ? LIMIT 50";
+            $comando = "SELECT codigoE, aditivo, peligro_aditivo.gradoPeligro, origen_aditivo.origen, "
+                    . "clasificacion_aditivo.clasificacion, descripcionAditivo, usoAditivo, "
+                    . "efectosSecundariosAditivo "
+                    . "FROM aditivos LEFT JOIN peligro_aditivo "
+                    . "ON aditivos.idPeligroAditivo = peligro_aditivo.idPeligroAditivo"
+                    . " LEFT JOIN origen_aditivo ON aditivos.idOrigenAditivo = origen_aditivo.idOrigenAditivo"
+                    . " LEFT JOIN clasificacion_aditivo ON aditivos.idClasificacionAditivo = clasificacion_aditivo.idClasificacionAditivo"
+                    . " WHERE aditivo LIKE ? OR codigoEBuscador LIKE ? LIMIT 50";
 //'7802820701210' así queda al hacerle bind
                 // Preparar sentencia
                 $sentencia = $pdo->prepare($comando);
@@ -125,6 +123,7 @@ class search
                 $queryFinal = "%" . $query . "%";
                 $queryFinalFinal = $queryFinal . "%";
                 $sentencia->bindParam(1, $queryFinal, PDO::PARAM_STR);
+                $sentencia->bindParam(2, $queryFinal, PDO::PARAM_STR);
 
             // Ejecutar sentencia preparada
             if ($sentencia->execute()) {
@@ -148,6 +147,7 @@ class search
             "Ocurrió el siguiente error al consultar las citas médicas: " . $e->getMessage());
         }
     }
+} 
 
     private static function retrieveSearchAllergy($leche,$gluten,$query){
         try {
