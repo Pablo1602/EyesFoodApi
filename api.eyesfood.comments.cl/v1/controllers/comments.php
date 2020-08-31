dcea7b6587fbe43ece7b3b20497e5d566e36ed09
Pablo <pablo.cancino@usach.cl>
2020-08-30 21:32:34 -0400

    comentario

--- api.eyesfood.comments.cl/v1/controllers/comments.php
+++ api.eyesfood.comments.cl/v1/controllers/comments.php
@@ -272,7 +272,7 @@ class comments
                 0,
                 "Error de base de datos en el servidor",
                 "http://localhost",
-                "Ocurrió el siguiente error al intentar insertar el usuario: " . $e->getMessage());
+                "Ocurrió el siguiente error al intentar insertar el comentario: " . $e->getMessage());
         }
     }
     private static function saveNewResponse($idComentario) {
